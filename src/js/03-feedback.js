b23e6f9632a84ef4596b2df6bc3ac66a04f25365
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-01-15 23:39:56 +0200

    Refactored code
    1) add condition that checking all fields are completed; 2) show only last value when click submit if all fields are complited

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -18,10 +18,6 @@ function storeDataOnInput (event) {
     messageValue,
   }
 
-  if (emailValue === "" && messageValue === "") {
-    return console.log("Please fill in all the fields!");
-  }
-
   users.push(userData)
   localStorageAPI.save('feedback-form-state', users);
 }
@@ -41,11 +37,23 @@ getUserData ()
 
 function hadlerDataOnSubmit (event) {
   event.preventDefault();
+  
+  if (users.length) {
+    if (users[users.length - 1].emailValue === "" 
+    || users[users.length - 1].messageValue === ""
+    ) {
+      return console.log("Please fill in all the fields!");
+    }else{
+    console.log(
+    'email:', users[users.length - 1].emailValue, 
+    'message:', users[users.length - 1].messageValue
+    )
 
-  console.log(users)
   localStorageAPI.remove('feedback-form-state')
   localStorage.clear('feedback-form-state')
   formEl.reset()
+    }
+  }
 }
 
 formEl.addEventListener('submit', throttle (hadlerDataOnSubmit, 1000))
